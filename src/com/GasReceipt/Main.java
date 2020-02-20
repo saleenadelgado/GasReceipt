97b627ac1a22b19eba5e741c98ef0f699afee596
saleenadelgado <Saleena.delgado.2022@student.lhschools.org>
2020-02-20 12:27:36 -0500

    intellija commit

---
+++ src/com/GasReceipt/Main.java
@@ -0,0 +1,25 @@
+package com.GasReceipt;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("+-----------------------------+");
+    System.out.println("|  Delgado's Gas Station      |");
+    System.out.println("|  3059 Glouchester rd        |");
+    System.out.println("|  Florida  32013             |");
+    System.out.println("|  Date              Time     |");
+    System.out.println("|  14Jun2020         3:22 PM  |");
+    System.out.println("|  Invoice Number#    123536  |");
+    System.out.println("|  Auth#                      |");
+    System.out.println("|  MasterCard                 |");
+    System.out.println("|  **************35           |");
+    System.out.println("|  pump      product    $/G   |");
+    System.out.println("|  01       unleaded    $2.65 |");
+    System.out.println("|  Amount    Fuel Total       |");
+    System.out.println("|  23.00       $60.95         |");
+    System.out.println("|                             |");
+    System.out.println("|         Thank You!          |");
+    System.out.println("|    Delgado's Gas Station    |");
+    System.out.println("+-----------------------------+");
+    }
+}
